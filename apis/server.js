0dcf288379989fbcef2ef517f0710ce11fe19c26
EErickson <avilasonson@gmail.com>
2024-01-16 00:52:14 +0800

    add another api route w/ refactoring

--- apis/server.js
+++ apis/server.js
@@ -9,11 +9,12 @@ const notesController = new NotesController();
 app.use(express.json());
 
 //Users API
-app.post('/add-user', (req,res) => userController.addUser(req,res));
-app.get('/users', (req,res)=> userController.fetchUsers(req,res));
+app.post(`/add-user`, (req,res) => userController.addUser(req, res));
+app.get(`/users`, (req,res)=> userController.fetchUsers(req, res));
 //Notes API
-app.post('/add-notes', (req, res) => notesController.addNotes(req, res));
-app.get('/all-notes',(req, res) => notesController.fetchAllNotes(req,res));
+app.post(`/add-notes`, (req, res) => notesController.addNotes(req, res));
+app.get(`/all-notes`,(req, res) => notesController.fetchAllNotes(req, res));
+app.get(`/note/:id`, (req, res) => notesController.fetchANote(req, res, req.params.id));
 
 const port = 5000
 app.listen(port, ()=>{
