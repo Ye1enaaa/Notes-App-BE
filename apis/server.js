6913581af1e5c4de22b3261880d9798fb5595aff
EErickson <avilasonson@gmail.com>
2024-01-15 23:56:58 +0800

    refactor routes with OOP

---
+++ apis/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const User = require('../models/Users');
+const UserController = require('../controllers/UserController');
+const NotesController = require('../controllers/NotesController');
+const app =  express();
+
+const userController = new UserController();
+const notesController = new NotesController();
+app.use(express.json());
+
+//Users API
+app.post('/add-user', (req,res) => userController.addUser(req,res));
+app.get('/users', (req,res)=> userController.fetchUsers(req,res));
+//Notes API
+app.post('/add-notes', (req, res) => notesController.addNotes(req, res));
+app.get('/all-notes',(req, res) => notesController.fetchAllNotes(req,res));
+
+const port = 5000
+app.listen(port, ()=>{
+    console.log(`Server Running on port ${port}`)
+})
