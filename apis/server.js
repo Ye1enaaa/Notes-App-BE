ad3862a1ac3a5b19d3fa4801c99e0bfb95d23aee
EErickson <avilasonson@gmail.com>
2024-01-16 01:28:40 +0800

    add patch route

--- apis/server.js
+++ apis/server.js
@@ -1,9 +1,7 @@
 const express = require('express');
-const User = require('../models/Users');
-const UserController = require('../controllers/UserController');
-const NotesController = require('../controllers/NotesController');
 const app =  express();
-
+const UserController = require('../controllers/UserController');
+const NotesController = require('../controllers/NotesController')
 const userController = new UserController();
 const notesController = new NotesController();
 app.use(express.json());
@@ -16,6 +14,7 @@ app.post(`/add-notes`, (req, res) => notesController.addNotes(req, res));
 app.get(`/all-notes`,(req, res) => notesController.fetchAllNotes(req, res));
 app.get(`/note/:id`, (req, res) => notesController.fetchANote(req, res, req.params.id));
 app.delete(`/delete-note/:id`, (req, res) => notesController.deleteNote(req, res, req.params.id));
+app.patch(`/edit-title/:id`, (req, res) => notesController.updateTitleOfNote(req, res, req.params.id));
 
 const port = 5000
 app.listen(port, ()=>{
